22cbce3737876426fa7e11901262434ef7038421
mincheol <homerjay@naver.com>
2023-08-29 01:31:02 +0900

    ADD : Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'package:my_squash/screens/navigation.dart';
+
 
 void main() {
   runApp(const MyApp());
